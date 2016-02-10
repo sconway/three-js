c64ba70520d4933ff454cfd400184ecd6146818a
Scott Conway <conway.sc@husy.neu.edu>
2016-02-10 00:17:28 -0500

    adding some more panels

--- plane/js/test-3js.js
+++ plane/js/test-3js.js
@@ -1,29 +1,32 @@
 // standard global variables
-var container, v, floor, scene, camera, renderer, controls, stats;
-var keyboard = new THREEx.KeyboardState();
-var clock = new THREE.Clock();
-// custom global variables
-var mesh;
+var container, v, floor, scene, camera, renderer, controls, stats,
+    keyboard = new THREEx.KeyboardState(),
+    clock = new THREE.Clock(),
+    mesh,
+    planeWidth = 2000, 
+    planeHeight = 900,
+    floorSize = planeWidth * 3,
+    VIEW_ANGLE = 45, 
+    ASPECT = window.innerWidth / window.innerHeight, 
+    NEAR = 0.1, 
+    FAR = 20000;
 
 init();
 animate();
 
-// FUNCTIONS        
+
+/**
+ * Sets up the necessary items for initialization. This includes
+ * the camera, renderer, scene, controls, etc. 
+ */      
 function init() {
     // SCENE
     scene = new THREE.Scene();
 
     // CAMERA
-    var SCREEN_WIDTH = window.innerWidth, 
-        SCREEN_HEIGHT = window.innerHeight;
-        VIEW_ANGLE = 45, 
-        ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, 
-        NEAR = 0.1, 
-        FAR = 20000;
-
     camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
     scene.add(camera);
-    camera.position.set(0, 20, SCREEN_HEIGHT*3);
+    camera.position.set(0, 20, floorSize/2);
     camera.lookAt(scene.position);
 
     // RENDERER
@@ -32,55 +35,125 @@ function init() {
     else
         renderer = new THREE.CanvasRenderer(); 
 
-    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    // Scales the content down to fit the screen size
+    renderer.setSize(window.innerWidth, window.innerHeight);
     container = document.getElementById( 'ThreeJS' );
     container.appendChild( renderer.domElement );
 
     controls = new THREE.OrbitControls( camera, renderer.domElement );
 
     // LIGHT
+    addLight();
+    addFloor();
+    addPanels();
+
+    var windowResize = new THREEx.WindowResize(renderer, camera);
+}
+
+
+function addLight() {
     var light = new THREE.PointLight(0xffffff);
     light.position.set(100,250,100);
     scene.add(light);
+}
 
-    // FLOOR
+function addFloor() {
     var loader = new THREE.TextureLoader().load('images/checkerboard.png');
     loader.wrapS = THREE.RepeatWrapping;
     loader.wrapT = THREE.RepeatWrapping;
     loader.repeat.set( 50, 50 );
+
     var floorMaterial = new THREE.MeshBasicMaterial( { map: loader, side: THREE.DoubleSide} );
-    var floorGeometry = new THREE.PlaneGeometry(SCREEN_WIDTH*3, SCREEN_WIDTH*3, 50, 50);
+    var floorGeometry = new THREE.PlaneGeometry(floorSize, floorSize, 50, 50);
     floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.position.y = -(SCREEN_HEIGHT/8);
+    floor.position.y = -(planeHeight/8);
     floor.rotation.x = Math.PI / 2;
     scene.add(floor);
-    scene.fog = new THREE.Fog( 0xf2f2f2, 0.1, FAR/10 );
+    scene.fog = new THREE.Fog( 0xf2f2f2, 0.1, floorSize);
 
-    makeCube(0,SCREEN_HEIGHT*2.25, -125, 10);
-    makeCube(400,500, -125, 10);
-    makeCube(-400,SCREEN_HEIGHT, -125, -10);
-    makeCube(0, 0, -125, 0);
-    makeCube(SCREEN_WIDTH*1.5, 0, -125, 90);
-
-
-    // SKYBOX
     var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
     var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0xf2f2f2, side: THREE.BackSide } );
     var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
     scene.add(skyBox);
-    new THREEx.WindowResize(renderer, camera);
+}
+
+
+function addPanels() {
+    makeCube(0,((floorSize/2) - 400), -150, 10);
+    makeCube(400,500, -150, 10);
+    makeCube(-400,planeHeight, -150, -10);
+    makeCube(0, 0, -150, 0);
+    makeCube(planeWidth*1.5, 0, -150, 90);
 }
 
 
 function makeCube(x,y,z, rotation) {
     var cube = new THREE.Mesh(
-            new THREE.BoxGeometry(200, 2, z*2),
-            new THREE.MeshLambertMaterial({ color: 0xff0000, })
+            new THREE.BoxGeometry(200, 2, 250),
+            new THREE.MeshLambertMaterial({ color: 0xff0000 })
         )
 
     cube.position.set(x, y, z);
     cube.rotation.z = rotation;
     floor.add(cube);
+}
+
+
+function handleFloorMovement() {
+    if ( keyboard.pressed("left") ) {
+        floor.rotation.z += 0.01;
+    }
+    if ( keyboard.pressed("right") ) { 
+        floor.rotation.z -= 0.01;
+    }
+    if ( keyboard.pressed("up") ) { 
+        console.log("Z value:", floor.position.z );
+        floor.position.z += 5;
+    }
+    if ( keyboard.pressed("down") ) { 
+        console.log("Z value:", floor.position.z );
+        floor.position.z -= 5;
+    }
+}
+
+
+function handleCameraMovement() {
+    if ( keyboard.pressed("q") ) { 
+        camera.position.x -= 1;
+    }
+    if ( keyboard.pressed("w") ) { 
+        camera.position.x += 1;
+    }
+    if ( keyboard.pressed("a") ) { 
+        camera.position.y -= 1;
+    }
+    if ( keyboard.pressed("s") ) { 
+        camera.position.y += 1;
+    }
+    if ( keyboard.pressed("z") ) { 
+        camera.position.z -= 1;
+    }
+    if ( keyboard.pressed("x") ) { 
+        camera.position.z += 1;
+    }
+    if ( keyboard.pressed("e") ) { 
+        camera.rotation.x -= 0.01;
+    }
+    if ( keyboard.pressed("r") ) { 
+        camera.rotation.x += 0.01;
+    }
+    if ( keyboard.pressed("d") ) { 
+        camera.rotation.y -= 0.01;
+    }
+    if ( keyboard.pressed("f") ) { 
+        camera.rotation.y += 0.01;
+    }
+    if ( keyboard.pressed("c") ) { 
+        camera.rotation.z -= 0.01;
+    }
+    if ( keyboard.pressed("v") ) { 
+        camera.rotation.z += 0.01;
+    }
 }
 
 
@@ -91,29 +164,8 @@ function animate() {
 }
 
 function update(){
-    if ( keyboard.pressed("left") ) {
-        floor.rotation.z += 0.01;
-    }
-    if ( keyboard.pressed("right") ) { 
-        floor.rotation.z -= 0.01;
-    }
-    if ( keyboard.pressed("q") ) { 
-        console.log("Y value:", floor.position.y );
-        floor.position.y -= 1;
-    }
-    if ( keyboard.pressed("w") ) { 
-        console.log("Y value:", floor.position.y );
-        floor.position.y += 1;
-    }
-    if ( keyboard.pressed("up") ) { 
-        console.log("Z value:", floor.position.z );
-        floor.position.z += 5;
-    }
-    if ( keyboard.pressed("down") ) { 
-        console.log("Z value:", floor.position.z );
-        floor.position.z -= 5;
-    }
-
+    handleFloorMovement();
+    handleCameraMovement();
     controls.update();
     // stats.update();
 }
