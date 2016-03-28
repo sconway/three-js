6c6ac77bcaf2734efbb4ce05596823d59e06e2fd
Scott Conway <conway.sc@husy.neu.edu>
2016-03-28 00:31:23 -0400

    mobile effects

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -32,6 +32,7 @@ var container,
 	unIntersectMutex = true,
 	isTweening = false,
 	moveCamera = false,
+	mouseDown = false,
 	rotateSphere = false,
 	scaleToX = window.innerWidth/50,
 	scaleToY = window.innerHeight/50,
@@ -43,6 +44,7 @@ var container,
 	projectInView = false,
 	radius = window.innerWidth/2, 
 	theta = 0,
+	spinTheta = 0.005,
 	cameraZ = 1500,
 	cubeSize = 50,
 	scaleToZ = cameraZ/cubeSize,
@@ -60,7 +62,8 @@ var container,
 	names  = [ "College Culture", "Copyright Clearance", "Father Peyton",
 			   "IHA Today", "Standish Mellon", "Wolf Greenfield", 
 			   "Zildjian Cymbals", "Enernoc" ],
-	loaders = [];
+	loaders = [],
+	objects = [];
 
 
 // init();
@@ -128,7 +131,14 @@ function init() {
 
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 	// window.addEventListener("devicemotion", handleMotion, false);
-	$(".canvas").click(function() { zoomToProject(); });
+	$(".canvas").click( function(event) { 
+		if ( window.innerWidth > 1000 ) {
+			zoomToProject();
+		} else {
+			mouseDown = true;
+			showProject(event);
+		}
+	});
 	$("#backBtn").click(function() { backToProjectView(); });
 	
 	var windowResize = new THREEx.WindowResize(renderer, camera);
@@ -173,34 +183,40 @@ function loadFont() {
  */
 function setTeaserContainer(current, x, y) {
 	var distance = camera.position.distanceTo(current.position);
+	console.log("distance: ", distance);
 
 	// make sure the mouse has been moved before dropping the
 	// teaser down. Prevents it from showing on load.
-	if ( x > 0 && y > 0 ) {
+	if ( (x > 0 && y > 0) || mouseDown ) {
 		$("#teaserName, #projectTitle").html(current.children[0].name);
 
-		if ( x > window.innerWidth/2 ) {
-			var xFactor = distance < 1500 ? 700 : 620;
+		if ( window.innerWidth > 1000 ) {
+			if ( x > window.innerWidth/2 ) {
+				var xFactor = distance < window.innerWidth ? 650 : 600;
 
-			// mouse on right bottom of screen (Q4)
-			if ( y > window.innerHeight/2 ) {
-				revealTeaser(x - xFactor, y - 275, true);
-			} 
-			// mouse on right top of screen (Q2)
-			else {
-		    	revealTeaser(x - xFactor, y - 60, false);
+				// mouse on right bottom of screen (Q4)
+				if ( y > window.innerHeight/2 ) {
+					revealTeaser(x - xFactor, y - 275);
+				} 
+				// mouse on right top of screen (Q2)
+				else {
+			    	revealTeaser(x - xFactor, y - 60);
+				}
+			} else {
+				var xFactor = distance < window.innerWidth ? 300 : 250;
+
+				// mouse on the bottom left of screen (Q3)
+				if ( curMouse.y > window.innerHeight/2 ) {
+					revealTeaser(x + xFactor, y - 275);
+				} 
+				// mouse on the top left of screen (Q4)
+				else {
+					revealTeaser(x + xFactor, y - 60);
+				}
 			}
 		} else {
-			var xFactor = distance < 1500 ? 350 : 310;
-
-			// mouse on the bottom left of screen (Q3)
-			if ( curMouse.y > window.innerHeight/2 ) {
-				revealTeaser(x + xFactor, y - 275, true);
-			} 
-			// mouse on the top left of screen (Q4)
-			else {
-				revealTeaser(x + xFactor, y - 60, false);
-			}
+			// just place the teaser in the bottom center when we are on mobile
+			revealTeaser(30, 200);
 		}
 	}
 }
@@ -224,73 +240,44 @@ function addShapes() {
 
 	for ( var i = 0; i < numImages; i++ ) {
 		var size     = ((i + 2) * 10) - ((i + 1) * 5),
-		    faceSize = rando(minFace, maxFace),
-		    numFaces = Math.random(),
 			material = new THREE.MeshBasicMaterial( {  
 							map: loaders[i], 
 							side: THREE.DoubleSide 
 						}),
-			color   = new THREE.MeshLambertMaterial( { 
-							color: 0xffffff * Math.random(),
-							side: THREE.DoubleSide,
-							transparent: true,
-							opacity: 1
-						}),
-			object = new THREE.Mesh( 
+			color    = new THREE.ShaderMaterial( {
+						    uniforms: 
+							{ 
+								"c": { type: "f", value: 1.0 },
+								"p": { type: "f", value: 1.0 },
+								glowColor: { type: "c", value: new THREE.Color(0xffffff * Math.random()) },
+								viewVector: { type: "v3", value: camera.position }
+							},
+							vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
+							fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+							side: THREE.FrontSide,
+							blending: THREE.AdditiveBlending,
+							transparent: true
+						} ),
+			moonGlow   = new THREE.Mesh( 
 						new THREE.SphereGeometry( 40, 64, 64 ),
-						color 
+						color.clone()
 						),
-			group  = new THREE.Object3D();
+			group    = new THREE.Object3D();
 
-
- 		group.name = "project_group";
-
-		object.rotation.x = Math.random() * 2 * Math.PI;
-		object.rotation.y = Math.random() * 2 * Math.PI;
-		object.rotation.z = Math.random() * 2 * Math.PI;
-		object.name = names[i];
-
-		text = createText(object.name, object.position);
-
-		// group.add( object );
-
-		// controls.push( new THREE.DeviceOrientationControls( object , true ) );
-
-
-		var sphereGeom = new THREE.SphereGeometry( 40, 64, 64 );
-		
-		// create custom material from the shader code above
-		//   that is within specially labeled script tags
-		var customMaterial = new THREE.ShaderMaterial( 
-		{
-		    uniforms: 
-			{ 
-				"c":   { type: "f", value: 1.0 },
-				"p":   { type: "f", value: 1.4 },
-				glowColor: { type: "c", value: new THREE.Color(0xffffff) },
-				viewVector: { type: "v3", value: camera.position }
-			},
-			vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
-			fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-			side: THREE.FrontSide,
-			blending: THREE.AdditiveBlending,
-			transparent: true
-		}   );
-			
-		var moonGlow = new THREE.Mesh( sphereGeom.clone(), customMaterial.clone() );
-	    // moonGlow.position = moon.position;
 		moonGlow.scale.multiplyScalar(1.2);
 		moonGlow.name = names[i];
-		group.add( moonGlow );
+ 		group.name = "project_group";
 
+		text = createText(moonGlow.name, moonGlow.position);
+
+		group.add( moonGlow );
 		group.add( text );
 
+		// controls.push( new THREE.DeviceOrientationControls( group , true ) );
+
+		objects.push(group);
 		scene.add( group );
 	}
-
-
-	
-
 
 
 	// since there won't be a current sphere on page load, set one initially,
@@ -367,7 +354,7 @@ function createText(text, pos) {
     	textGeom = new THREE.TextGeometry( text , 
     	{
     		font: font,
-    		size: 14.0,
+    		size: window.innerWidth > 1000 ? 14.0 : 22.0,
     		height: 50.0,
     		bevelThickness: 4.0
     	}),
@@ -407,6 +394,10 @@ function hideText() {
 }
 
 
+/*
+ * Sets the text mesh for each project group to be visible. Iterates
+ * through the children in the scene to find the various project groups.
+ */
 function showNames() {
 	var numChildren = scene.clone().children.length;
 
@@ -425,23 +416,15 @@ function showNames() {
  * @param      pos     :     Vector3
  *
  */
-function revealTeaser(x, y, slideUp) {
+function revealTeaser(x, y) {
 	if (INTERSECTED) {
-		if (slideUp) {
-			$("#teaser")
-				.css({
-					left: x + "px",
-					top:  y + "px"
-				})
-				.addClass("active");
-		} else {
-			$("#teaser")
-				.css({
-					left: x + "px",
-					top:  y + "px"
-				})
-				.addClass("active");	
-		}
+		console.log("revealing Teaser");
+		$("#teaser")
+			.css({
+				left: x + "px",
+				top:  y + "px"
+			})
+			.addClass("active");	
 	}
 }
 
@@ -463,6 +446,36 @@ function handleMotion(event) {
 
   // Do stuff with the new orientation data
   console.log(gamma);
+}
+
+
+/**
+ * Called after a click on a mobile device. It detects if there are any
+ * objects clicked on, and passes those objects to a function that handles
+ * them. A function to reset the scene is called if there are no objects
+ * clicked on.
+ */
+function showProject(event) {
+	event.preventDefault();
+
+	curMouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+	curMouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+
+	raycaster.setFromCamera( mouse, camera );
+
+	var intersects = raycaster.intersectObjects( objects, true );
+
+	// Make sure there are intersects.
+	if ( intersects.length > 0 ) {
+		if ( curSphere === intersects[0].object  ) {
+			zoomToProject();
+		} else {
+			onMobileIntersection(intersects);
+		}
+	} else {
+		onNoMobileIntersection();
+	}
+
 }
 
 
@@ -534,8 +547,8 @@ function spinCamera() {
  */
 function spinSphere() {
 	if (curSphere) {
-		curSphere.parent.rotation.x -= 0.005;
-	    curSphere.parent.rotation.y -= 0.005;
+		curSphere.parent.rotation.x -= spinTheta;
+	    curSphere.parent.rotation.y -= spinTheta;
 	}
 }
 
@@ -603,8 +616,8 @@ function spheresToRandom() {
 	// Tween all project spheres, starting at the second child, since the
 	// first child in the scene is the light.
 	for (var i = 1; i < numChildren; i++) {
-		var posX = (i + 1) * 60 * (i % 2 === 0 ? -1 : 1),
-			posY = Math.random() * 800 - 400,
+		var posX = window.innerWidth > 1000 ? (i + 1) * 60 * (i % 2 === 0 ? -1 : 1) : rando(-60, -40),
+			posY = window.innerWidth > 1000 ? (Math.random() * 800 - 400) : (i * 70 - (i * 4)) * (i % 2 === 0 ? -1 : 1),
 			posZ = Math.random() * 800 - 500,
 			vector = new THREE.Vector3(posX, posY, posZ);
 
@@ -622,19 +635,12 @@ function spheresToRandom() {
 				.easing( TWEEN.Easing.Elastic.InOut )
 				.onStart( function() {
 					isTweening = true;
-					// curSphere.material = new THREE.MeshLambertMaterial( { 
-					// 							color: 0xffffff * Math.random(),
-					// 							side: THREE.DoubleSide,
-					// 							transparent: true,
-					// 							opacity: 0.9
-					// 						});
-					curSphere.material = new THREE.ShaderMaterial( 
-						{
+					curSphere.material = new THREE.ShaderMaterial( {
 						    uniforms: 
 							{ 
 								"c":   { type: "f", value: 1.0 },
-								"p":   { type: "f", value: 1.4 },
-								glowColor: { type: "c", value: new THREE.Color(0xffffff) },
+								"p":   { type: "f", value: 1.0 },
+								glowColor: { type: "c", value: new THREE.Color(0xffffff * Math.random()) },
 								viewVector: { type: "v3", value: camera.position }
 							},
 							vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
@@ -649,32 +655,12 @@ function spheresToRandom() {
 			    	renderer.render(scene, camera);
 			    })
 			    .onComplete( function() {
+			    	// called once when the tween completes
 			    	if ( isTweening ) {
 			    		isTweening = false;
-			    		// curSphere.material = new THREE.MeshLambertMaterial( { 
-									// 			color: 0xffffff * Math.random(),
-									// 			side: THREE.DoubleSide,
-									// 			transparent: true,
-									// 			opacity: 0.9
-									// 		});
-						// curSphere.material = new THREE.ShaderMaterial( 
-						// {
-						//     uniforms: 
-						// 	{ 
-						// 		"c":   { type: "f", value: 1.0 },
-						// 		"p":   { type: "f", value: 1.4 },
-						// 		glowColor: { type: "c", value: new THREE.Color(0xffffff) },
-						// 		viewVector: { type: "v3", value: camera.position }
-						// 	},
-						// 	vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
-						// 	fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-						// 	side: THREE.FrontSide,
-						// 	blending: THREE.AdditiveBlending,
-						// 	transparent: true
-						// } );
+			    		shrinkSphere();
 				    	$("#teaser").removeClass("active");
 			    		showNames();
-				    	console.log("Done tweening; adding project names. index is: ", i);
 			    	}
 			    	
 			    })
@@ -692,16 +678,17 @@ function spheresToRandom() {
  *
  */
 function expandSphere(object) {
-	console.log("expand sphere called. object is: ", object);
-	var numChildren = scene.clone().children.length;
+	var numChildren = scene.clone().children.length,
+		scaleSize   = window.innerWidth > 1000 ? 8 : 4,
+		sphere      = object.children[0];
 
-	console.log(object);
+	console.log("expand sphere called: ", object);
 
 	new TWEEN.Tween(object.scale)
 		.to({
-			x: 8,
-			y: 8,
-			z: 8
+			x: scaleSize,
+			y: scaleSize,
+			z: scaleSize
 		}, 500)
 		.easing( TWEEN.Easing.Circular.Out )
 	    .onStart( function() {
@@ -711,14 +698,14 @@ function expandSphere(object) {
 	    	renderer.render(scene, camera);
 	    })
 	    .onComplete( function() {
-	    	console.log("done expanding sphere. Adding video texture.");
 	    	// object.material = new THREE.MeshBasicMaterial( {  
 						// 	map: loaders[names.indexOf(object.name)], 
 						// 	side: THREE.DoubleSide 
 						// });
-		    console.log(object);
-			object.material = addVideo(object.name);
-	    	rotateSphere = true;
+			if ( window.innerWidth > 1000 ) {
+				sphere.material = addVideo(sphere.name);
+	    		rotateSphere = true;
+	    	}
 	    })
 	    .start();
 }
@@ -728,10 +715,10 @@ function expandSphere(object) {
  * Scales the supplied object down by tweening the scale property.
  */
 function shrinkSphere() {
-	var numChildren = scene.children.length;
+	var numChildren = objects.length;
 
-	for (var i = 1; i < numChildren; i++) {
-		new TWEEN.Tween(scene.children[i].children[0].scale)
+	for (var i = 0; i < numChildren; i++) {
+		new TWEEN.Tween(objects[i].scale)
 			.to({
 				x: 1,
 				y: 1,
@@ -745,6 +732,32 @@ function shrinkSphere() {
 		    })
 		    .start();
 	}
+}
+
+
+/**
+ * This function is called after a click on a mobile device. It moves
+ * the clicked sphere up to the top and calls the other spheres to move
+ * towards it. 
+ *
+ * @param      sphere     :     THREE.Object
+ *
+ */
+function moveSphereUp(sphere) {
+	new TWEEN.Tween(sphere.position)
+		.to({
+			x: 0,
+			y: 400,
+			z: 0
+		}, 750)
+		.easing( TWEEN.Easing.Circular.Out )
+	    .onUpdate( function() {
+	    	renderer.render(scene, camera);
+	    })
+	    .onComplete( function() {
+	    	spheresToCurrent(sphere);
+	    })
+	    .start();
 }
 
 
@@ -764,12 +777,12 @@ function zoomToSelection(target) {
 		}, 3000)
 		.easing( TWEEN.Easing.Linear.None )
 	    .onUpdate( function() {
-	    	console.log("tweening camera in");
 	    	// camera.lookAt(selectedProject.position);
 	    	renderer.render(scene, camera);
 	    })
 	    .onComplete( function() {
 	    	rotateCamera = true;
+	    	spinTheta = 0.001;
 			$(".project-details, .project-intro, .canvas").addClass("active");
 	    })
 	    .start();
@@ -791,14 +804,13 @@ function zoomCameraOut() {
 		}, 3000)
 		.easing( TWEEN.Easing.Linear.None )
 	    .onUpdate( function() {
-	    	console.log("tweening camera out");
 	    	camera.lookAt(scene.position);
 	    	renderer.render(scene, camera);
 	    })
 	    .onComplete( function() {
-	    	console.log("done zooming out");
 	    	selectedProject = null;
 			projectInView = false;
+			spinTheta = 0.005;
 			TWEEN.removeAll();
 			spheresToRandom();
 	    })
@@ -882,31 +894,75 @@ function onIntersection(intersects) {
 		children      = scene.clone().children,
 		numChildren   = children.length;
 
-		console.log("INTERSECTED: ", INTERSECTED);
-		console.log("CURRENT: ", curSphere);
-
 	// only run this if the intersected 
- 	if ( INTERSECTED != intersects[0].object && 
- 		 !isTweening &&
+ 	if ( INTERSECTED != intersects[0].object && !isTweening && 
  		 intersects[0].object.name != "project_name" &&
  		 curMouse.x !== 0 && curMouse.y !== 0 ) {
 
 		$("html").css({cursor: 'pointer'});
 		curSphere = INTERSECTED = intersects[0].object;
 
-		if (!projectInView) {
+		if ( !projectInView ) {
 			stopCamera = true;
 			unIntersectMutex = true;
 		}
 
-		if (intersectMutex) {
+		if ( intersectMutex ) {
 			intersectMutex = false;
 			hideText();
-			expandSphere(curSphere);
+			expandSphere(curSphere.parent);
 			spheresToCurrent(curSphere.parent);
 		}
 	}
 }
+
+
+
+function onMobileIntersection(intersects) {
+
+    curSphere = INTERSECTED = intersects[0].object;
+
+	if ( !projectInView ) {
+		stopCamera = true;
+		unIntersectMutex = true;
+	}
+
+	// Only call this once, and if a click has been made.
+	// Prevents it from being called initially.
+	if ( intersectMutex ) {
+		console.log("mobile intersection");
+		intersectMutex = false;
+		hideText();
+		expandSphere(curSphere.parent);
+		moveSphereUp(curSphere.parent);
+		// spheresToCurrent(curSphere.parent);
+	}
+
+}
+
+
+function onNoMobileIntersection() {
+
+	// Don't do anything if there's a project in view.
+	if ( !projectInView ){
+		stopCamera     = false;
+		intersectMutex = true;
+		// Only call this once, and if a click has been made.
+		// Prevents it from being called initially since there
+		// will be no intersections on load.
+		if ( unIntersectMutex ) {
+			console.log("no intersections");
+			spheresToRandom();
+			shrinkSphere();
+			if ( video ) video.pause();
+			rotateSphere = false;
+			unIntersectMutex = false;
+		}
+	}
+	
+	INTERSECTED = null;
+}
+
 
 
 /*
@@ -916,13 +972,14 @@ function onIntersection(intersects) {
  */
 function onNoIntersections(intersects) {
 	
+	// Don't do anything if there's a project in view.
 	if ( !projectInView ){
 		stopCamera     = false;
 		intersectMutex = true;
-		if (unIntersectMutex) {
+		if ( unIntersectMutex ) {
 			spheresToRandom();
 			shrinkSphere();
-			if (video) video.pause();
+			if ( video ) video.pause();
 			rotateSphere = false;
 			unIntersectMutex = false;
 		}
@@ -950,27 +1007,26 @@ function animate() {
 function render() {
 	// cirlcle the camera around as long as there isn't a project
 	// in view or one being hovered on.
-	if (!projectInView && !stopCamera) {
+	if ( !projectInView && !stopCamera ) {
 		circleCamera();
 	}
 
-	if (rotateCamera) {
+	if ( rotateCamera ) {
 	    spinCamera();
 	}
 
-	if (rotateSphere) {
+	if ( rotateSphere ) {
 		spinSphere();
 	}
 
 	// updates the sample video 
-	if (video) {
+	if ( video ) {
 		if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
 			videoImageContext.drawImage( video, 0, 0 );
 			if ( videoTexture ) 
 				videoTexture.needsUpdate = true;
 		}
 	}
-	
 
 
 	// find intersections
@@ -979,11 +1035,20 @@ function render() {
 	var intersects = raycaster.intersectObjects( scene.children, true );
 
 	// Check if the mouse pointer has intersected any of the objects
-	if ( intersects.length > 0 ) {
-		onIntersection(intersects);
+	if ( window.innerWidth > 1000 ) {
+		if ( intersects.length > 0 ) {
+			onIntersection(intersects);
+		} else {
+			onNoIntersections();
+		}
 	} else {
-		onNoIntersections();
+		// if ( intersects.length > 0 ) {
+		// 	onMobileIntersection(intersects);
+		// } else {
+		// 	onNoMobileIntersection();
+		// }
 	}
+	
 
 	renderer.render( scene, camera );
 }
